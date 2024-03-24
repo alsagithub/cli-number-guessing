a97960cdeaa8c66a63341e6994228f10ccea15bc
Samia <162901845+alsagithub@users.noreply.github.com>
2024-03-24 20:50:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+// 1 computer will generate a rondom number -----done
+//2 user input for gusseing number  --done
+//3 computer user input with computer generat number and show result
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("WELCOME,to  number guesing Game");
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-6: ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congtratulation! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number..");
+}
